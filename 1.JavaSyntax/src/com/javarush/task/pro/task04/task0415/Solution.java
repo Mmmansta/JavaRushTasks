5d4f37e0044e08988894f3f3df47a1eb97a8737a
Mmmansta <128576312+Mmmansta@users.noreply.github.com>
2023-03-27 12:34:28 +0300

    add Solution java syntax from task 0415

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0415;
+
+import java.util.Scanner;
+
+/* 
+Площадь круга
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int radius = scan.nextInt();
+       double pi = 3.14;
+       int area = (int) Math.floor(pi*radius*radius);
+        System.out.println(area);
+
+    }
+}
