7fb1dab9a615a669bb3c1a36672edf8c8ed88758
Mmmansta <128576312+Mmmansta@users.noreply.github.com>
2023-04-24 21:40:25 +0300

    add Solution java syntax from task 0802

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0802/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task08.task0802;
+
+/* 
+Утильный класс: часть 2
+*/
+
+public class Solution {
+
+    public static double sqrt(double a) {
+        double sqrt = Math.sqrt(a);
+        return sqrt;
+    }
+
+    public static double cbrt(double a) {
+        double cbrt = Math.cbrt(a);
+        return cbrt;
+    }
+
+    public static double pow(int number, int power) {
+        double pow = Math.pow(number, power);
+        return pow;
+        }
+    }
+
