3b6d937121340eab50db2b47bb0221c9afdf5e22
Mmmansta <128576312+Mmmansta@users.noreply.github.com>
2023-04-26 19:04:48 +0300

    add Solution java syntax from task 0622

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = new String();
+        String b = new String();
+        String c = new String();
+        String d = new String();
+        String e = new String();
+        String f = new String();
+        String g = new String();
+        String i = new String();
+        String j = new String();
+        String k = new String();
+        int ab[] = new int[5];
+        int[] cd = new int[4];
+        int[] dc = new int[3];
+        int[] ba = new int[2];
+        int[] va = new int[1];
+        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+    }
+}
